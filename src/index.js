b1120009b77f580bab452443776cd99466976726
maevanapcontact <maevanap.contact@gmail.com>
2019-06-28 23:16:11 +0200

    chore: basic configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+/*eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
